ef251169991f73e5089207fcf67d0c7789a7adbc
Hillary Ramirez <187204710+HillaryRam@users.noreply.github.com>
2026-02-02 11:51:31 +0100

    Upload project files

---
+++ resources/js/Layouts/Layouts.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Header from '@/Layouts/Header.vue';
+import Footer from '@/Layouts/Footer.vue';
+import Nav from '@/Layouts/Nav.vue';
+</script>
+
+<template>
+    <Nav />
+    <Header />
+    <slot />
+    <Footer />
+</template>
+<style scoped>
+
+</style>
