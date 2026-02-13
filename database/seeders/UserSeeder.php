cc7adc33f82fe47d06ccfe1a767d44b0fb16f094
Hillary Ramirez <187204710+HillaryRam@users.noreply.github.com>
2026-02-13 11:02:00 +0100

    crecion de roles de usuario

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $user = User::factory([
+            'name' => 'Admin',
+            'email' => 'a@a.com',
+            'password' => bcrypt('12345678'),
+        ]);
+        $user->assignRole('admin');
+        User::factory()->count(5)->create()->each(function ($user) {
+            $user->assignRole('student'); //5 usuarios estudiantes
+        });
+        User::factory()->count(5)->create()->each(function ($user) {
+            $user->assignRole('teacher'); //5 usuarios profesores
+        });
+        //
+    }
+}
